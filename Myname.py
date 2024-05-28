6c56e002a5b6006cad0801b4c1e400ae6d26b630
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-05-27 23:35:03 -0300

    Criando uma variável

---
+++ Myname.py
@@ -0,0 +1,7 @@
+character_name = ('Gustavo')
+character_age = '20'
+
+print('Meu nome é ' + character_name)
+print('Eu tenho ' + character_age + ' anos') 
+print('Gosto realmente do nome ' + character_name)
+print('Para ' + character_age + ' anos' ' me formei bem cedo na faculdade')
