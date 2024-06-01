78455cf69828f231fd9c5fb95e9303c5250b3af4
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-06-01 15:20:23 -0300

    Create a game madlibs

---
+++ Jogo-madlibs.py
@@ -0,0 +1,7 @@
+cor = input('Digite uma cor: ')
+substantivo_plural = input('Digite um substantivo no plural: ')
+celebridade = input('Digite uma celebridade: ')
+
+print('Rosas são ' + cor)
+print(substantivo_plural + ' são azuis')
+print('Eu amo ' + celebridade)
