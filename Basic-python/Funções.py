828a571048c3a5007bf72d20221fdd7c3f5918cf
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-06-01 17:05:00 -0300

    Return

---
+++ Basic-python/Funções.py
@@ -0,0 +1,5 @@
+def diga_ola(nome, idade):
+    print('Olá ' + nome + ', você tem ' + str(idade) + ' anos')
+
+diga_ola('Gustavo', '20')    
+diga_ola('Bruno', '20') 
---
+++ Basic-python/Retornar.py
@@ -0,0 +1,5 @@
+def cube(numero):
+    return numero*numero*numero
+
+resultado = cube(7)
+print(resultado)
