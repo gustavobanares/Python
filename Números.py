1e801a7397e5753f7b65e5adbeed3e2313ab9f42
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-06-01 13:55:57 -0300

    Use numbers

---
+++ Números.py
@@ -0,0 +1,37 @@
+#Soma básica
+print(7 + 7)
+
+#Conta com prioridade no parenteses
+print(7 * (8 + 1))
+
+#Declarando um número como variável str 
+meu_numero = 7
+print(str(meu_numero) + ' é meu número favorito')
+
+#Número absoluto
+segundo_numero = -10
+print(abs(segundo_numero)) 
+
+#Potência 
+print(pow(2, 5))
+
+#Maior número
+print(max(2, 5))
+
+#Menor número
+print(min(2, 5))
+
+#Arredondar número
+print(round(4.5))
+
+#Importando a função matemática dando mais acessos para funções
+from math import *
+
+#Pegar o menor número
+print(floor(2.7))
+
+#Arrendodar o número
+print(ceil(2.7))
+
+#Raiz quadrada
+print(sqrt(14))
