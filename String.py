b3b2906c70f4ef5a30628da77028433c86777dd1
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-06-01 13:30:51 -0300

    Little of string

---
+++ String.py
@@ -0,0 +1,12 @@
+frase = 'Academia do Bairro'
+print(frase + ' é grande')
+
+print(frase.upper().isupper())
+
+print(len(frase))
+
+print(frase[4])
+
+print(frase.index('e'))
+
+print(frase.replace('Bairro', 'Gustavo'))
