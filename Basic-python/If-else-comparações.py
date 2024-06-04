158269117a96455cff6f4137729baca2bbc14fe5
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-06-03 22:01:20 -0300

    Corrigindo um código na hora de revisar

--- Basic-python/If-else-comparações.py
+++ Basic-python/If-else-comparações.py
@@ -2,7 +2,7 @@ def maior_numero(numero1, numero2, numero3):
     if numero1 >= numero2 and numero1 >= numero3:
         return numero1
     elif numero2 >= numero1 and numero2 >= numero3:
-        return numero1
+        return numero2
     else:
         return numero3
     
