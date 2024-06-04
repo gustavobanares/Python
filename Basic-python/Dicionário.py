4563ca28964623dda459c82d70d7cf8546c0d2b8
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-06-03 22:33:05 -0300

    Criando um dicionário

---
+++ Basic-python/Dicionário.py
@@ -0,0 +1,16 @@
+conversão_de_mês = {
+    'Jan': 'Janeiro',
+    'Fev': 'Fevereiro',
+    'Mar': 'Março',
+    'Abr': 'Abril',
+    'Maio': 'Maio',
+    'Jun': 'Junho',
+    'Jul': 'Julho',
+    'Ago': 'Agosto',
+    'Set': 'Setembro',
+    'Out': 'Outubro',
+    'Nov': 'Novembro',
+    'Dez': 'Dezembro',
+}
+
+print(conversão_de_mês.get('Oct', 'Não é uma chave válida'))
