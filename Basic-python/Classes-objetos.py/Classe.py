2c4f55065f3e283e316021abecd6719dd18d2a02
Gustavo Bañares de Bacco <101230170+gustavobanares@users.noreply.github.com>
2024-06-11 17:58:42 -0300

    Class and objects

---
+++ Basic-python/Classes-objetos.py/Classe.py
@@ -0,0 +1,7 @@
+class Estudante:
+    def __init__(self, nome, especialização, ra, está_aprovado):
+        self.nome = nome
+        self.especialização = especialização
+        self.ra = ra
+        self.está_aprovado = está_aprovado
+    
---
+++ Basic-python/Classes-objetos.py/Objeto.py
@@ -0,0 +1,6 @@
+from Classe import Estudante
+
+estudante1 = Estudante('Gustavo', 'Tecnologia', '7.0', False)
+estudante2 = Estudante('Guilherme', 'Nutrição', '2.8', True)
+
+print(estudante2.nome)
